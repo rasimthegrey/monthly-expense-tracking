8f7c7ca5e66ed34ff89a8da07e612320ae0627c9
rasimthegrey <rasim.mutlu@protonmail.com>
2021-09-14 01:08:24 +0300

    add expenses

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses = [
+    {
+      id: 1,
+      title: "Desk",
+      price: 75,
+      date: new Date(2021, 9, 8)
+    },
+    {
+      id: 2,
+      title: "Mousepad",
+      price: 7,
+      date: new Date(2021, 9, 8)
+    }
+  ]
+
   return (
     <div className="App">
-      <h1>
-        hello
-      </h1>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].price} date={expenses[0].date}/>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].price} date={expenses[1].date}/>
     </div>
   );
 }
