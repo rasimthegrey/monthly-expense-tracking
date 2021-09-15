c520cbf10f96a945fe952fd14902b67a458cdfaf
rasimthegrey <rasim.mutlu@protonmail.com>
2021-09-15 12:55:03 +0300

    fixed typo

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -8,7 +8,7 @@ const ExpenseDate = (props) => {
 
     return (
         <div className="expense-date">
-            <div classname="expense-date-month">{month}</div>
+            <div className="expense-date-month">{month}</div>
             <div className="expense-date-day">{day}</div>
             <div className="expense-date-year">{year}</div>
         </div>
