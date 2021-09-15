6eb46030dfb30a22fd53e8b215b36946913479e1
rasimthegrey <rasim.mutlu@protonmail.com>
2021-09-15 14:04:45 +0300

    ui update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
 
   return (
     <div className="App">
+      <NewExpense />
       <Expenses items={expenses}/>
       
     </div>
